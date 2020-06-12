ab4ce03c6ee4610f5a26323a6771c69353ea20f8
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-12 11:22:19 -0700

    add controller methods to park_controller.

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,12 +1,36 @@
 class NationalParksConroller < ApplicationController
 
   def index
-    @national_parks = {"national_park": "Hot Springs National Park"}
+    @national_parks = NationalPark.all
     json_response(@national_parks)
+  end
+
+  def show
+    @national_park = NationalPark.find(params[:id])
+    json_response(@national_park)
+  end
+
+  def create
+    @national_park = NationalPark.create(park_params)
+    json_response(@national_park)
+  end
+
+  def update
+    @national_park = NationalPark.find(params[:id])
+    @national_park.update(park_params)
+  end
+
+  def destroy
+    @national_park = NationalPark.find(params[:id])
+    @national_park.destroy
   end
 
   private
     def json_response(object, status = :ok)
       render json: object status: status
+    end
+
+    def park_params
+      params.permit(:national_park, :state)
     end
 end
