f29d38b1473cb388c589f4e21b8bd11704398ed1
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-12 16:19:16 -0700

    troubleshoot cause of KeyError: Factory not registered.

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory(:national_parks) do
+    national_park { "A National Park" }
+    state { "A State" }
+  end
+end
---
+++ spec/requests/get_parks_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "get all parks route", :type => :request do
+  let!(:national_parks) { FactoryBot.create_list(:national_park, 20)}
+  
+  before { get '/national_parks' }
+
+  it 'returns all parks' do
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
