29aa9e4cc16ba4a405b8013424441e2fadc6cb47
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-23 15:51:14 -0700

    add new test for returning the name of national park. Test fails.

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -1,1 +1,12 @@
 require 'rails_helper'
+
+describe "post a national park route", :type => :request do
+  
+  before do
+    post '/national_parks', params: { :national_park => "Yellow Stone", :state => "Montana"}
+  end
+
+  it 'returns the name of the national park' do
+    expect(JSON.parse(response.body)['national_park']).to eq('heh?')
+  end
+end
