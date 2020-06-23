e3cd6903371aa33801ac09378af8c343a059d57f
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-06-23 14:01:28 -0700

    add a test for 200 and ok check on successfull deletion.

--- spec/requests/delete_route_spec.rb
+++ spec/requests/delete_route_spec.rb
@@ -11,4 +11,8 @@ describe 'delete route', :type => :request do
   it 'deletes a national park' do
     expect(JSON.parse(response.body)).to eq({"message" => "National Park successfully deleted!"})
   end
+
+  it 'returns status 200 ok when delete is successfull' do
+    expect(response).to have_http_status(:ok)
+  end
 end
